4f290266f1dda7ad05eaa39f25ba34f67ff0d914
Ben Joseph <benjoseph101@gmail.com>
2023-10-26 18:21:12 +0100

    Added basic search function

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,19 +1,28 @@
 ﻿using System.Runtime.InteropServices;
 using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
 
+//- bot stuck in repetition
+//- need piece square table for evaluation
+//- need alpha beta pruning
+//- need sorting of evaluated moves to spee up alpha beta pruning
 
 public class MyBot : IChessBot
 {
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    Move thinkmove = Move.NullMove;
 
     public Move Think(Board board, Timer timer)
     {
-        Move[] allMoves = board.GetLegalMoves();
-        int eval = Evaluate(board);
-        return allMoves[0];
-
+        // incorporate basic alpha beta search algorithm using basic evaluation function
+        // basic evaluation function can be updated iteratively throughout 
+        Move thinkmove = Move.NullMove;
+        int eval = Search(3, 0, -10000, 10000, board);
+        return thinkmove.IsNull ? board.GetLegalMoves()[0] :thinkmove;
     }
 
     // Test if this move gives checkmate
@@ -26,6 +35,8 @@ public class MyBot : IChessBot
     }
 
     int Evaluate(Board board) {
+        // To reduce number of tokens, evaluation could be done iterating over all pieces
+        // This will also help to incorporate the piece square tables
         int whiteEval = CountMaterial (board, true);
         int blackEval = CountMaterial (board, false);
 
@@ -45,4 +56,39 @@ public class MyBot : IChessBot
 
         return material;
     }
-}
+
+    int Search(int depth, int ply, int alpha, int beta, Board board){
+        
+        if (ply > 0 && board.IsRepeatedPosition()) {
+            return 0;
+        }
+
+        if (depth == 0) {
+            return Evaluate(board);
+        }
+
+        Move[] allMoves = board.GetLegalMoves();
+        if (allMoves.Length == 0) {
+            if (board.IsInCheck()) {
+                return -10000;
+            }
+            return 0;
+        }
+
+        int bestEval = -10000;
+        Move bestMove = Move.NullMove;
+        foreach(Move move in allMoves){
+            board.MakeMove(move);
+            int eval = -Search(depth - 1, ply + 1, -beta, -alpha, board);
+            if (eval > bestEval){
+                bestEval = eval;
+                bestMove = move;
+                if (ply == 0) thinkmove = move;
+            }
+            board.UndoMove(move);
+        }
+
+        return bestEval;
+    }
+
+    }
